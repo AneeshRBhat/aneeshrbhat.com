192dd38a0ad41ea76941a45bf4cfa96a911a0b74
AneeshRBhat <aneeshbhatr@gmail.com>
2023-05-10 21:29:26 +0530

    merged main and for-designing. Revamped the design

--- markdown-blog/src/index.js
+++ markdown-blog/src/index.js
@@ -12,6 +12,7 @@ import Post from './pages/post';
 import NotFound from './pages/notFound';
 import Blog from './pages/blog';
 
+//This is it
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>

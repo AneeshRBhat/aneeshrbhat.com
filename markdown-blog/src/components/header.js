91b132ff18f79177841f95a0faaca803506418c1
AneeshRBhat <aneeshbhatr@gmail.com>
2023-05-10 17:45:07 +0530

    removed dummies and updated jsons

--- markdown-blog/src/components/header.js
+++ markdown-blog/src/components/header.js
@@ -30,10 +30,10 @@ const Header = () => {
 
     return(
         <div>
-            <div className="px-10 lg:px-20 md:px-16 bg-blue-950 heropattern-topography-yellow-500/30 flex justify-around gap-4 py-12 items-center ">
+            <div className="px-10 lg:px-20 md:px-16 bg-blue-950 heropattern-topography-blue-500/30 flex justify-around gap-4 py-12 items-center ">
                 <Logo/>
                 <Navbar className="hidder"/>
-                <svg onClick={hamClickHandler} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#F8FAFC" className="sm:w-10 w-8 block text-slate-50 block lg:hidden ">
+                <svg onClick={hamClickHandler} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#F8FAFC" className="sm:w-10 w-8 block text-slate-50 lg:hidden ">
                     <path fillRule="evenodd" d="M3 6.75A.75.75 0 013.75 6h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 6.75zM3 12a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 12zm0 5.25a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75a.75.75 0 01-.75-.75z" clipRule="evenodd" />
                 </svg>
             </div>
